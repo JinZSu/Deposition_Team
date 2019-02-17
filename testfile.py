20949e89b14a969edb102b955ae3e28f5cad30ce
Jin <js9202@nyu.edu>
2019-02-16 22:13:21 -0500

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,4 +1,13 @@
-def HelloJin():
-	return
-sklfjeoiw
-ASsaSAs
+def elevation():
+#need to find out how the levetation wroks
+
+def vibration():
+#activate vibration motors
+
+def weight_sensor():
+#need to read the weight sensor
+
+def math_weight():
+#prototype testing to know when the robot is functioning
+
+#???? That's all I got so far
