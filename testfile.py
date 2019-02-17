a694cf08488501670704d134276a46182485b53b
Ziyao Shangguan <ziyaoshangguan@10-18-83-211.dynapool.wireless.nyu.edu>
2019-02-17 15:14:29 -0500

    Feb 17

--- testfile.py
+++ testfile.py
@@ -1,13 +1,47 @@
-def elevation():
-#need to find out how the levetation wroks
+class deposition:
+	def __init__(self):
+		self.vibration = False
+		# position z is when storage is ready to deposit gravel
+		self.pos_z = 45
+		# position x is when we deposit to the bin
+		self.pos_x = 55
+		# position y is the lowest bin position
+		self.pos_y = 0
+		self.curr_pos = 0
+		self.weight = 50
+		# when rack is false, it is at original position
+		self.rack = False
 
-def vibration():
-#activate vibration motors
+	def actuator(self, target_pos):
+		diff = self.curr_target - target_pos
+		if diff >=  0:
+			print "Actuator going up"
+		else:
+			print "Actuator going down"
 
-def weight_sensor():
-#need to read the weight sensor
+	def vibration(self):
+		if self.vibration:
+			print "Turning off vibration"
+			self.vibration = False
+		else:
+			print "Turning on vibration"
+			self.vibration = True
 
-def math_weight():
-#prototype testing to know when the robot is functioning
+	def deposition(self, purpose):
+		if purpose == "collect":
+			self.actuator(self.pos_z)
+			self.vibration()
+		elif purpose == "deposit":
+			self.actuator(self.pos_x)
+			self.push()
+		self.actuator.pos_y()
 
-#???? That's all I got so far
+	def push(self):
+		print "Rack and pinion pushing"
+
+	
+	
+		
+		 
+		
+    		
